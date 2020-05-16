7d06bff6d4a8efad9f776214bbe45415db188c4c
Jacob Jabr <jacob_jabr@hotmail.com>
2020-05-16 14:39:49 +0200

    ratings add, and minor changes

--- scrapingCPU.py
+++ scrapingCPU.py
@@ -6,21 +6,25 @@ from os import write
 driver = webdriver.Chrome()
 
 with open('cpu.csv', 'w') as f:
-    f.write("CpuName, price \n")
+    f.write("CpuName, price, rating \n")
 
-    url = "https://www.komplett.dk/category/11204/hardware/pc-komponenter/processorer?nlevel=10000%C2%A728003%C2%A711204&hits=2000"
+    url = "https://www.komplett.dk/category/11204/hardware/pc-komponenter/processorer?nlevel=10000%C2%A728003%C2%A711204&hits=100"
     driver.get(url)
     html = driver.page_source
-    content = BeautifulSoup(html)
+    page_content = BeautifulSoup(html, features="lxml")
 
 with open('cpu.csv', 'a') as f:
 
-    for x in content.findAll("div", attrs={"class": "content-block"}):
-        names = x.findAll("a", attrs={"class": "product-link"})
-        prices = x.findAll("div", attrs={"class": "product-price"})
+    for content_block in page_content.findAll("div", attrs={"class": "content-block"}):
+        products_info = content_block.findAll("a", attrs={"class": "product-link"})
+        products_price = content_block.findAll("div", attrs={"class": "product-price"})
+        products_review = content_block.findAll("div", attrs={"class": "review"})
         #print(names)
-        for name, price in zip(names, prices):
+        for name, price, rating in zip(products_info, products_price, products_review):
+            print(name)
             n = name.findAll("h2")
+            print(n)
             p = price.findAll("span")
-            for finalPrice, finalName in zip(p, n):
-                f.write(finalName.text + "," + finalPrice.text + "\n")
+            r = rating.findAll("span", attrs={"class": "rating-stars"})
+            for finalPrice, finalName, finalRating in zip(p, n, r):
+                f.write(finalName.text + "," + finalPrice.text +  "," + finalRating["title"] + "\n")
--- scrapingRam.py
+++ scrapingRam.py
@@ -6,12 +6,12 @@ from os import write
 driver = webdriver.Chrome()
 
 with open('ram.csv', 'w') as f:
-    f.write("RamName, price \n")
+    f.write("RamName, price, rating \n")
 
-    url = "https://www.komplett.dk/category/11209/hardware/pc-komponenter/hukommelse-ram?nlevel=10000%C2%A728003%C2%A711209&hits=2000"
+    url = "https://www.komplett.dk/category/11209/hardware/pc-komponenter/hukommelse-ram?nlevel=10000%C2%A728003%C2%A711209&hits=100"
     driver.get(url)
     html = driver.page_source
-    content = BeautifulSoup(html)
+    content = BeautifulSoup(html, features="lxml")
 
 with open('ram.csv', 'a') as f:
     # for x in content.findAll("a", attrs={"class": "product-link"}):
@@ -26,12 +26,16 @@ with open('ram.csv', 'a') as f:
     #         print(price.text)
     #         #f.write(name.text + "\n")
 
-    for x in content.findAll("div", attrs={"class": "content-block"}):
-        names = x.findAll("a", attrs={"class": "product-link"})
-        prices = x.findAll("div", attrs={"class": "product-price"})
+    for content_block in content.findAll("div", attrs={"class": "content-block"}):
+        names = content_block.findAll("a", attrs={"class": "product-link"})
+        prices = content_block.findAll("div", attrs={"class": "product-price"})
+        products_review = content_block.findAll("div", attrs={"class": "review"})
+
         # print(names)
-        for name, price in zip(names, prices):
+        for name, price, rating in zip(names, prices, products_review):
             n = name.findAll("h2")
             p = price.findAll("span")
-            for finalPrice, finalName in zip(p, n):
-                f.write(finalName.text + "," + finalPrice.text + "\n")
+            r = rating.findAll("span", attrs={"class": "rating-stars"})
+
+            for finalPrice, finalName, finalRating in zip(p, n, r):
+                f.write(finalName.text + "," + finalPrice.text + "," + finalRating["title"] + "\n")
